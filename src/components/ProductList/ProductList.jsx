f1eac11f53f37fd544f0f1204db37b9ac97e3d09
Виталий Анофриев <deyn1995@mail.ru>
2023-02-27 12:34:56 +0300

    Ищу ошибку

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,10 +31,10 @@ const ProductList = () => {
             queryId
         }
 
-        fetch('https://5.178.85.176:8000/web-data', {
+        fetch('http://5.178.85.176:8000/web-data', {
             method: 'POST',
             headers: {
-                'Content-type' : 'application/json',
+                'Content-type': 'application/json',
             },
             body: JSON.stringify(data)
         });
